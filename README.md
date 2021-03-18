4d6818b42c89fc32c444b06510f2913e0c6a0311
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-03-17 20:21:36 -0500

    Changed scripts.js and readme.md
    sciptst.js:
    Initial objects created and added to list
    array
    
    readme.md:
    Initial comments added with purpose and source URL

---
+++ README.md
@@ -0,0 +1,6 @@
+App Purpose:
+The purpose of this app is to display information related to ski resorts across the world.  Clicking on a resort will bring up further information on it.
+
+Project API:
+Skimap.org API
+URL: https://skimap.org/pages/Developers
--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,32 @@
-alert('Hello world');
+<script>
+    let resortList = [];
 
-let favoriteFood='Pizza';
-document.write(favoriteFood);
+    let Alyeska={
+        name: 'Alyeska Resort',
+        region: 'Alaska',
+        elevationTop: 1201,
+        elevationBottom: 107,
+        elevationDrop: 1201-107,
+    };
+
+    let Breckenridge={
+        name: 'Breckenridge',
+        region: 'Colorado',
+        elevationTop: 3962,
+        elevationBottom: 2926,
+        elevationDrop: 3962-2926,
+    };
+
+    let Stratton={
+        name: 'Stratton Mountain Resort',
+        region: 'Vermont',
+        elevationTop: 1181,
+        elevationBottom: 609,
+        elevationDrop: 1181-609,
+    };
+
+    resortList[0]=Alyeska;
+    resortList[1]=Breckenridge;
+    resortList[2]=Stratton;
+
+</script>
