f40f1a723008a4ac1c6d642a350cd715f2069508
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-03-23 10:23:20 -0500

    Update scripts.js
    Populated resortList directly rather than with variables.

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,27 @@
 <script>
     let resortList = [];
 
-    let Alyeska={
-        name: 'Alyeska Resort',
-        region: 'Alaska',
-        elevationTop: 1201,
-        elevationBottom: 107,
-        elevationDrop: 1201-107,
-    };
-
-    let Breckenridge={
-        name: 'Breckenridge',
-        region: 'Colorado',
-        elevationTop: 3962,
-        elevationBottom: 2926,
-        elevationDrop: 3962-2926,
-    };
-
-    let Stratton={
-        name: 'Stratton Mountain Resort',
-        region: 'Vermont',
-        elevationTop: 1181,
-        elevationBottom: 609,
-        elevationDrop: 1181-609,
-    };
-
-    resortList[0]=Alyeska;
-    resortList[1]=Breckenridge;
-    resortList[2]=Stratton;
-
+    let resortList=[
+        {
+            name: 'Alyeska Resort',
+            region: 'Alaska',
+            elevationTop: 1201,
+            elevationBottom: 107,
+            elevationDrop: 1201-107,
+        },
+        {
+            name: 'Breckenridge',
+            region: 'Colorado',
+            elevationTop: 3962,
+            elevationBottom: 2926,
+            elevationDrop: 3962-2926,
+        },
+        {
+            name: 'Stratton Mountain Resort',
+            region: 'Vermont',
+            elevationTop: 1181,
+            elevationBottom: 609,
+            elevationDrop: 1181-609,
+        }
+    ]    
 </script>
