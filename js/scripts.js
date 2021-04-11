3ab8edc80ebb577ea91b5678c7f676b386d3f18d
rosswyman <76174453+rosswyman@users.noreply.github.com>
2021-04-11 10:08:23 -0500

    Update scripts.js
    Temporarily replace add function while working through other code.  The boolean functionality will need to be reimplemented.

--- js/scripts.js
+++ js/scripts.js
@@ -5,30 +5,34 @@ let quakeRepository=(function(){
     let quakeList = []; 
 
 
-    
-    // adds a new quake to the end of quakeRepository
+    // ! -- THIS FUNCTION HAS BEEN REPLACED WITH A SIMPLIFIED VERSION BELOW WHILE WORKING THROUGH OTHER PARTS OF THE CODE -- !
+    // // adds a new quake to the end of quakeRepository
+    // function add(quake){
+    //     // Checks to see if the parameter being passed to the add function is an object
+    //     if (typeof(quake) === 'object'){
+    //         // These are the keys expected to be passed to the add function
+    //         let expectedKeys=[
+    //         'name',
+    //         'region',
+    //         'elevationTop',
+    //         'elevationBottom',
+    //         'elevationDrop'
+    //         ];
+    //         // Creates an array of the keys of the object passed to the add function
+    //         let testKeys=Object.keys(quake);
+    //         // Checks to see if keys match
+    //         if(JSON.stringify(expectedKeys)==JSON.stringify(testKeys)){
+    //           quakeList.push(quake);
+    //         } else{
+    //         alert('That is not allowed.  You have attempted to add incorrect quake information');
+    //       }      
+    //     } else{
+    //       alert('That is not allowed.  You have attempted to add incorrect quake information');
+    // }}
+
     function add(quake){
-        // Checks to see if the parameter being passed to the add function is an object
-        if (typeof(quake) === 'object'){
-            // These are the keys expected to be passed to the add function
-            let expectedKeys=[
-            'name',
-            'region',
-            'elevationTop',
-            'elevationBottom',
-            'elevationDrop'
-            ];
-            // Creates an array of the keys of the object passed to the add function
-            let testKeys=Object.keys(quake);
-            // Checks to see if keys match
-            if(JSON.stringify(expectedKeys)==JSON.stringify(testKeys)){
-              quakeList.push(quake);
-            } else{
-            alert('That is not allowed.  You have attempted to add incorrect quake information');
-          }      
-        } else{
-          alert('That is not allowed.  You have attempted to add incorrect quake information');
-    }}
+        quakeList.push(quake);
+      }
 
     // returns the list of resorts
     function getAll(){
